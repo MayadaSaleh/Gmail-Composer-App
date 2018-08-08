7e7e7d440f6c99d525ce6d3f6f86c704e17142de
Mayada Saleh <eng.mayadasaleh@gmail.com>
2018-08-08 03:32:12 +0200

    Splash Screen

---
+++ app/src/main/java/task/enozom/gmailcomposerapp/splash/screen/SplashScreen.java
@@ -0,0 +1,36 @@
+package task.enozom.gmailcomposerapp.splash.screen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import task.enozom.gmailcomposerapp.MainActivity;
+import task.enozom.gmailcomposerapp.R;
+
+public class SplashScreen extends Activity {
+
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+            setContentView(R.layout.activity_splash_screen);
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    //  will be executed once the timer is over
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                    // close this activity
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+
+        }
+
+    }
